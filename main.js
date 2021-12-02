71cf2bc45f4fcb43c5b3b2cc6b566912f5aa1ed4
alessiobocci <alessio.bocci@nivi.it>
2021-12-02 18:39:55 +0100

    risoluzione problema di una sola Riga N4 dopo N2 nel file CSV (non c'è la riga della sanzione ridotta)

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ fs.readdir(filePath, function (err, files) {
                 return
             }
 
-            let csvFile = extractData(data, activities);
+            let csvFile = extractData(data, activities, file);
             saveCsvData(filePath + 'output\\' + file, csvFile);
         });
 
@@ -58,8 +58,7 @@ fs.readdir(filePath, function (err, files) {
 });
 
 
-
-function extractData(data, activities) {
+function extractData(data, activities, fileName) {
 
     let array = data.split(/\r\n|\n/)
 
@@ -76,24 +75,42 @@ function extractData(data, activities) {
         dataNotifica: ''
     }
 
+    function NumeroVerbaliPerFile() {
+        numeroVerbali: '';
+        nomeFile: '';
+    }
+
     let rigaFile = '';
-    
+
     let finalData = {
         csvData: [],
-        dataCount: []
+        dataCount: [
+
+        ]
     }
 
     let csvData = [];
 
     var verbale = new Verbale();
+    var numeroVerbaliPerFile = new NumeroVerbaliPerFile();
+    numeroVerbaliPerFile.numeroVerbale = 0;
     // do-while loops
     let i = 0;
     let x = array.length;
+
+    let numeroVerbali = 0;
     do {
         let line = array[i].substring(0, 2);
 
+        if (line.includes('MGNMLL58E67L206Z'))
+            var a = 0;
+
+        // console.log(i + " prima di N2")
         if (line == 'N2') {
+            numeroVerbaliPerFile.numeroVerbale += 1;
+            // console.log(i + " dentro N2")
             rigaFile = line;
+
             verbale.codiceFiscaleConducente = String(line == 'N2' ? array[i].substring(activities.codiceFiscaleConducente[0], activities.codiceFiscaleConducente[1]) : '').replace(/\s+/g, ' ').trim()
             verbale.nomeCognomeConducente = String(line == 'N2' ? array[i].substring(activities.nomeCognomeConducente[0], activities.nomeCognomeConducente[1]) : '').replace(/\s+/g, ' ').trim()
 
@@ -109,104 +126,125 @@ function extractData(data, activities) {
         }
 
         if (line == 'N3') {
+            // console.log(i + " dentro  N3")
             rigaFile = line;
             verbale.codiceFiscaleProprietario = String(line == 'N3' ? array[i].substring(activities.codiceFiscaleProprietario[0], activities.codiceFiscaleProprietario[1]) : '').trim();
             verbale.nomeCognomeProprietario = String(line == 'N3' ? array[i].substring(activities.nomeCognomeProprietario[0], activities.nomeCognomeProprietario[1]) : '').trim();
         }
 
         if (line == 'N4') {
-            if (rigaFile == line) {
+
+            if (rigaFile == 'N4') {
+                i++;
+                rigaFile = line;
+                continue;
+            }
+            // if (rigaFile == line) {
+            //     console.log(i + " dentro  N4 se rigaFile=line")
+            //     csvData.push(verbale);
+            //     var verbale = new Verbale();
+            //     i++;
+            //     continue;
+            // } else {
+
+            // console.log(i + " dentro  N4 ")
+            //importo
+            verbale.importo = String(line == 'N4' ? array[i].substring(activities.importo[0], activities.importo[1]) : '').trim();
+
+            //numeroVerbale
+            if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length <= 5)
+                verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1] - 2) : '').trim();
+            else if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length > 7) {
+                verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1] + 1) : '').trim();
+            } else
+                verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').trim();
+
+            if (verbale.numeroVerbale.length >= 12 && (verbale.numeroVerbale.substring(3).includes('P') == false || verbale.numeroVerbale.substring(3).includes('V') == false))
+                verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1] + 4) : '').trim();
+
+            if ((String(verbale.numeroVerbale).indexOf('P', 4) + 1) != 0) {
+                if ((String(verbale.numeroVerbale).indexOf('P', 4) + 1) != verbale.numeroVerbale.length)
+                    verbale.numeroVerbale = verbale.numeroVerbale.split('P')[0] + 'P';
+            }
+            if ((String(verbale.numeroVerbale).indexOf('V', 4) + 1) != 0) {
+                if ((String(verbale.numeroVerbale).indexOf('V', 4) + 1) != verbale.numeroVerbale.length)
+                    verbale.numeroVerbale = verbale.numeroVerbale.split('V')[0] + 'V';
+            }
+
+            //dataVerbale
+            if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length <= 5)
+                verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0] - 2, activities.dataVerbale[1] - 2) : '').trim();
+            else if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length >= 8)
+                verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0] + 2, activities.dataVerbale[1] + 2) : '').trim();
+            else if (typeof (line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1]) : '').split('P')[1] == 'undefined')
+                verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1]) : '').split('P')[0].trim();
+            else
+                verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1]) : '').split('P')[1].trim();
+
+            if (verbale.dataVerbale.includes('V ') || verbale.dataVerbale.includes('P '))
+                verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1] + 4) : '').split('P ')[1].trim();
+
+
+            //targa
+            if ((line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1]) : '').includes('.'))
+                verbale.targa = String(line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1]) : '').split('.')[1].trim();
+            else
+                verbale.targa = String(line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1]) : '').trim();
+
+            if (String(verbale.targa).includes(' A'))
+                verbale.targa = String(verbale.targa).split(' A')[0].trim()
+
+            if (verbale.targa.length <= 5)
+                verbale.targa = String(line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1] + 4) : '').split('.')[1].trim();
+
+            //articoloCDS
+            verbale.articoloCDS = (line == 'N4' ? array[i].substring(activities.articoloCDS[0], activities.articoloCDS[1]) : '').split('D')[0];
+
+            if (String(verbale.articoloCDS).lastIndexOf('.') == verbale.articoloCDS.length)
+                verbale.articoloCDS = (line == 'N4' ? array[i].substring(activities.articoloCDS[0], activities.articoloCDS[1] + 3) : '').split('D')[0];
+
+            verbale.articoloCDS = 'Art.' + verbale.articoloCDS.split('.')[1]
+
+
+            //dataNotifica sottocasi particolari
+
+            if (typeof (line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').split('t')[1] == 'undefined')
+                verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').split('t')[0])).trim();
+            else
+                verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').split('t')[1])).trim();
+
+            if (verbale.dataNotifica == 'No//') {
+                if (array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]).includes(' ') == true) {
+                    verbale.dataNotifica = (line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').trim();
+                    verbale.dataNotifica = String(convertData(verbale.dataNotifica.substr(verbale.dataNotifica.length - 8)));
+                }
+            }
+            if (verbale.dataNotifica.length != 10)
+                verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 2, activities.dataNotifica[1]) : ''))).trim();
+
+
+            //caso in cui ho solo 7 caratteri nella data (devono essere 8)
+            if ((line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').trim().length == 7) {
+
+                if (typeof (line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 1, activities.dataNotifica[1]) : '').split('t')[1] == 'undefined')
+                    verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 1, activities.dataNotifica[1]) : '').split('t')[0])).trim();
+                else
+                    verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 1, activities.dataNotifica[1]) : '').split('t')[1])).trim();
+
+
+            }
+
+            if (rigaFile == 'N2' || rigaFile == 'N3') {
+                // console.log(i + " dentro  N4 se rigaFile=line")
                 csvData.push(verbale);
                 var verbale = new Verbale();
                 i++;
+                rigaFile = line;
+
                 continue;
-            } else {
-                //importo
-                verbale.importo = String(line == 'N4' ? array[i].substring(activities.importo[0], activities.importo[1]) : '').trim();
-
-                //numeroVerbale
-                if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length <= 5)
-                    verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1] - 2) : '').trim();
-                else if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length > 7) {
-                    verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1] + 1) : '').trim();
-                } else
-                    verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').trim();
-
-                if (verbale.numeroVerbale.length >= 12 && (verbale.numeroVerbale.substring(3).includes('P') == false || verbale.numeroVerbale.substring(3).includes('V') == false))
-                    verbale.numeroVerbale = String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1] + 4) : '').trim();
-
-                if ((String(verbale.numeroVerbale).indexOf('P', 4) + 1) != 0) {
-                    if ((String(verbale.numeroVerbale).indexOf('P', 4) + 1) != verbale.numeroVerbale.length)
-                        verbale.numeroVerbale = verbale.numeroVerbale.split('P')[0] + 'P';
-                }
-                if ((String(verbale.numeroVerbale).indexOf('V', 4) + 1) != 0) {
-                    if ((String(verbale.numeroVerbale).indexOf('V', 4) + 1) != verbale.numeroVerbale.length)
-                        verbale.numeroVerbale = verbale.numeroVerbale.split('V')[0] + 'V';
-                }
-
-                //dataVerbale
-                if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length <= 5)
-                    verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0] - 2, activities.dataVerbale[1] - 2) : '').trim();
-                else if (String(line == 'N4' ? array[i].substring(activities.numeroVerbale[0], activities.numeroVerbale[1]) : '').split('/')[0].length >= 8)
-                    verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0] + 2, activities.dataVerbale[1] + 2) : '').trim();
-                else if (typeof (line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1]) : '').split('P')[1] == 'undefined')
-                    verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1]) : '').split('P')[0].trim();
-                else
-                    verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1]) : '').split('P')[1].trim();
-
-                if (verbale.dataVerbale.includes('V ') || verbale.dataVerbale.includes('P '))
-                    verbale.dataVerbale = String(line == 'N4' ? array[i].substring(activities.dataVerbale[0], activities.dataVerbale[1] + 4) : '').split('P ')[1].trim();
-
-
-                //targa
-                if ((line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1]) : '').includes('.'))
-                    verbale.targa = String(line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1]) : '').split('.')[1].trim();
-                else
-                    verbale.targa = String(line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1]) : '').trim();
-
-                if (String(verbale.targa).includes(' A'))
-                    verbale.targa = String(verbale.targa).split(' A')[0].trim()
-
-                if (verbale.targa.length <= 5)
-                    verbale.targa = String(line == 'N4' ? array[i].substring(activities.targa[0], activities.targa[1] + 4) : '').split('.')[1].trim();
-
-                //articoloCDS
-                verbale.articoloCDS = (line == 'N4' ? array[i].substring(activities.articoloCDS[0], activities.articoloCDS[1]) : '').split('D')[0];
-
-                if (String(verbale.articoloCDS).lastIndexOf('.') == verbale.articoloCDS.length)
-                    verbale.articoloCDS = (line == 'N4' ? array[i].substring(activities.articoloCDS[0], activities.articoloCDS[1] + 3) : '').split('D')[0];
-
-                verbale.articoloCDS = 'Art.' + verbale.articoloCDS.split('.')[1]
-
-
-                //dataNotifica sottocasi particolari
-
-                if (typeof (line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').split('t')[1] == 'undefined')
-                    verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').split('t')[0])).trim();
-                else
-                    verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').split('t')[1])).trim();
-
-                if (verbale.dataNotifica == 'No//') {
-                    if (array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]).includes(' ') == true) {
-                        verbale.dataNotifica = (line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').trim();
-                        verbale.dataNotifica = String(convertData(verbale.dataNotifica.substr(verbale.dataNotifica.length - 8)));
-                    }
-                }
-                if (verbale.dataNotifica.length != 10)
-                    verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 2, activities.dataNotifica[1]) : ''))).trim();
-
-
-                //caso in cui ho solo 7 caratteri nella data (devono essere 8)
-                if ((line == 'N4' ? array[i].substring(activities.dataNotifica[0], activities.dataNotifica[1]) : '').trim().length == 7) {
-
-                    if (typeof (line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 1, activities.dataNotifica[1]) : '').split('t')[1] == 'undefined')
-                        verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 1, activities.dataNotifica[1]) : '').split('t')[0])).trim();
-                    else
-                        verbale.dataNotifica = String(convertData((line == 'N4' ? array[i].substring(activities.dataNotifica[0] - 1, activities.dataNotifica[1]) : '').split('t')[1])).trim();
-
-                }
 
             }
+
         }
 
         rigaFile = array[i].substring(0, 2);
@@ -214,6 +252,8 @@ function extractData(data, activities) {
 
     }
     while (i < x);
+    numeroVerbaliPerFile.nomeFile = fileName;
+    console.log(numeroVerbaliPerFile);
     return csvData;
 };
 
